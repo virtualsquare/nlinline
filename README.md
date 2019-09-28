7f8c9a15ffe5a2e23a33d532fd9a7ee1beb519e5
Renzo Davoli <renzo@cs.unibo.it>
2019-09-28 18:13:49 +0200

    Initial release

--- README.md
+++ README.md
@@ -1,2 +1,67 @@
 # nlinline
 A quick and clean API for NetLink networking configuring.
+
+NLINLINE (netlink inline) is a *library* of inline functions providing C programmers with very handy functions to configure network stacks. NLINLINE is entirely implemented in a header file, nlinline.h.
+
+NLinline provides the following functions:
+
+*  `nlinline_if_nametoindex(const char *ifname);` return the index of the network interface whose name is `ifname`
+
+*  `int nlinline_linksetupdown(unsigned int ifindex, int updown);` turn the interface `ifindex` up (`updown == 1`) or down (`updown == 0`).
+
+*  `int nlinline_ipaddr_add(int family, void *addr, int prefixlen, int ifindex)` add an IP address to the interface `ifindex`. It supports IPv4 (`family == AF_INET`) and IPv6 `(family == AF_INET6)`.
+
+*  `int nlinline_ipaddr_del(int family, void *addr, int prefixlen, int ifindex)` remove the IP address from the interface `ifindex`. It supports IPv4 (`family == AF_INET`) and IPv6 `(family == AF_INET6)`.
+
+*  `int nlinline_iproute_add(int family, void *dst_addr, int dst_prefixlen, void *gw_addr);` add a static route to `dst_addr`/`dst_prefixlen` network through the gateway `gw_addr`. If `dst_addr == NULL` it adds a default route.
+
+*  `int nlinline_iproute_del(int family, void *dst_addr, int dst_prefixlen, void *gw_addr);` remove the static route to `dst_addr`/`dst_prefixlen` network through the gateway `gw_addr`. 
+
+IP addresses are `void *` arguments, any sequence of 4 or 16 bytes (in network byte order) is a legal IPv4 or IPv6 address respectively.
+
+### Netlink + inline
+
+* NLINLINE is a simple way to configure networking for network namespaces and *Internet of Threads* programs.
+* NLINLINE do not add dependencies at run-time. It is useful for security critical applications (like PAM modules)
+* NLINLINE uses netlink only, it does not depends on the obsolete netdevice (ioctl) API.
+* Only the code of referenced inline functions enters in the object and executable code.
+
+### Example
+
+```C
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include <nlinline.h>
+
+int main(int argc, char *argv[]) {
+  uint8_t ipv4addr[] = {192,168,2,2};
+  uint8_t ipv4gw[] = {192,168,2,1};
+  uint8_t ipv6addr[16] = {0x20, 0x01, 0x07, 0x60, [15] = 0x02};
+  uint8_t ipv6gw[16] = {0x20, 0x01, 0x07, 0x60, [15] = 0x01};
+
+  int ifindex = nlinline_if_nametoindex(argv[1]);
+  if (ifindex > 0)
+    printf("%d\n", ifindex);
+  else {
+    perror("nametoindex");
+    return 1;
+  }
+
+  if (nlinline_linksetupdown(ifindex, 1) < 0)
+    perror("link up");
+  if (nlinline_ipaddr_add(AF_INET, ipv4addr, 24, ifindex) < 0)
+    perror("addr ipv4");
+  if (nlinline_iproute_add(AF_INET, NULL, 0, ipv4gw) < 0)
+    perror("addr ipv6");
+  if (nlinline_ipaddr_add(AF_INET6, ipv6addr, 64, ifindex) < 0)
+    perror("route ipv4");
+  if (nlinline_iproute_add(AF_INET6, NULL, 0, ipv6gw) < 0)
+    perror("route ipv6");
+  return 0;
+}
+```
+
+This program takes the name of an interface from the command line. It turns that interface up and
+sets the interface IPv4 and IPv6 addresses and default routes.
+
---
+++ nlinline.h
@@ -0,0 +1,252 @@
+/*
+ * nlinline: compact library of inline function providing the most common
+ * network configuration operations.
+ *
+ * Copyright (C) 2019  Renzo Davoli <renzo@cs.unibo.it> VirtualSquare team.
+ *
+ * nlinline is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; If not, see <http://www.gnu.org/licenses/>. 
+ *
+ */
+
+#ifndef NLINLINE_H
+#define NLINLINE_H
+
+#include <stdio.h>
+#include <unistd.h>
+#include <errno.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <linux/if.h>
+#include <linux/if_link.h>
+#include <linux/if_addr.h>
+#include <linux/netlink.h>
+#include <linux/rtnetlink.h>
+
+static inline int nlinline_if_nametoindex(const char *ifname);
+static inline int nlinline_linksetupdown(unsigned int ifindex, int updown);
+
+static inline int nlinline_ipaddr_add(int family, void *addr, int prefixlen, int ifindex);
+static inline int nlinline_ipaddr_del(int family, void *addr, int prefixlen, int ifindex);
+
+static inline int nlinline_iproute_add(int family, void *dst_addr, int dst_prefixlen, void *gw_addr);
+static inline int nlinline_iproute_del(int family, void *dst_addr, int dst_prefixlen, void *gw_addr);
+
+
+/**************************
+ * Implementation
+ **************************/
+
+static inline int nlinline_family2addrlen(int family) {
+	switch (family) {
+		case AF_INET: return 4;
+		case AF_INET6: return 16;
+		default: return 0;
+	}
+}
+
+static inline int __nlinline_geterror(int fd) {
+	struct {
+		struct nlmsghdr h;
+		union {
+			struct nlmsgerr e;
+			struct ifinfomsg i;
+		};
+	} msg;
+	int replylen = recv(fd, &msg, sizeof(msg), 0);
+	if (replylen < 0)
+		return -1;
+	if (replylen <= sizeof(msg.h))
+		return errno = EFAULT, -1;
+	switch (msg.h.nlmsg_type) {
+		case NLMSG_ERROR: return (msg.e.error == 0) ? 0 : (errno = -msg.e.error, -1);
+		case RTM_NEWLINK: return msg.i.ifi_index;
+		default:          return errno = EFAULT, -1;
+	}
+}
+
+static inline int __nlinline_conversation(void *msg) {
+	struct nlmsghdr *nlmsg = msg;
+	struct sockaddr_nl sanl = {AF_NETLINK, 0, 0, 0};
+  int ret_value;
+  int fd = socket(AF_NETLINK, SOCK_RAW | SOCK_CLOEXEC, NETLINK_ROUTE);
+  if (fd < 0)
+    return fd;
+  if (bind(fd, (struct sockaddr *) &sanl, sizeof(struct sockaddr_nl)) < 0)
+    return close(fd), -1;
+  if (send(fd, msg, nlmsg->nlmsg_len, 0) < 0)
+    return close(fd), -1;
+  ret_value = __nlinline_geterror(fd);
+  close(fd);
+  return ret_value;
+}
+
+static inline int nlinline_if_nametoindex(const char *ifname) {
+	struct {
+    struct nlmsghdr h;
+    struct ifinfomsg i;
+		struct nlattr a;
+		char ifname[IFNAMSIZ];
+  } msg = {
+    .h.nlmsg_len = sizeof(msg.h) + sizeof(msg.i),
+    .h.nlmsg_type = RTM_GETLINK,
+    .h.nlmsg_flags = NLM_F_REQUEST | NLM_F_ACK,
+    .h.nlmsg_seq = 1,
+		.a.nla_type = IFLA_IFNAME,
+	};
+	int namelen = snprintf(msg.ifname, IFNAMSIZ, "%s", ifname);
+	msg.a.nla_len = sizeof(msg.a) + namelen + 1;
+	msg.h.nlmsg_len += (msg.a.nla_len + 3) & ~3;
+	return __nlinline_conversation(&msg);
+}
+
+static inline int nlinline_linksetupdown(unsigned int ifindex, int updown) {
+	struct {
+		struct nlmsghdr h;
+		struct ifinfomsg i;
+	} msg = {
+		.h.nlmsg_len = sizeof(msg),
+		.h.nlmsg_type = RTM_SETLINK,
+		.h.nlmsg_flags = NLM_F_REQUEST | NLM_F_ACK,
+		.h.nlmsg_seq = 1,
+		.i.ifi_index = ifindex,
+		.i.ifi_flags = (updown) ? IFF_UP : 0,
+		.i.ifi_change=IFF_UP };
+	return __nlinline_conversation(&msg);
+}
+
+struct __nlinline_ipv4addr {
+	unsigned char byte[4];
+};
+
+struct __nlinline_ipv6addr {
+	unsigned char byte[16];
+};
+
+struct __nlinline_ipv4attr {
+	struct nlattr h;
+	struct __nlinline_ipv4addr addr;
+};
+
+struct __nlinline_ipv6attr {
+	struct nlattr h;
+	struct __nlinline_ipv6addr addr;
+};
+
+static inline int __nlinline_ipaddr(int request, int xflags, int family, void *addr, int prefixlen, int ifindex) {
+	int addrlen = nlinline_family2addrlen(family);
+	if (addrlen == 0)
+		return errno = EINVAL, -1;
+	else {
+		struct {
+			struct nlmsghdr h;
+			struct ifaddrmsg i;
+			union {
+				struct __nlinline_ipv4attr a4[2];
+				struct __nlinline_ipv6attr a6[2];
+			};
+		}	msg = {
+			.h.nlmsg_len = sizeof(msg.h) + sizeof(msg.i),
+			.h.nlmsg_type = request,
+			.h.nlmsg_flags =  NLM_F_REQUEST | NLM_F_ACK | xflags,
+			.h.nlmsg_seq = 1,
+			.i.ifa_family = family,
+			.i.ifa_prefixlen = prefixlen,
+			.i.ifa_scope = RT_SCOPE_UNIVERSE,
+			.i.ifa_index = ifindex};
+		if (addrlen == 4) {
+			msg.a4[0].h.nla_len = msg.a4[1].h.nla_len = sizeof(struct nlattr) + addrlen;
+			msg.a4[0].h.nla_type = IFA_LOCAL;
+			msg.a4[1].h.nla_type = IFA_ADDRESS;
+			msg.a4[0].addr = msg.a4[1].addr = *((struct __nlinline_ipv4addr *) addr);
+			msg.h.nlmsg_len += 2 * sizeof(msg.a4[0]);
+		} else {
+			msg.a6[0].h.nla_len = msg.a6[1].h.nla_len = sizeof(struct nlattr) + addrlen;
+			msg.a6[0].h.nla_type = IFA_LOCAL;
+			msg.a6[1].h.nla_type = IFA_ADDRESS;
+			msg.a6[0].addr = msg.a6[1].addr = *((struct __nlinline_ipv6addr *) addr);
+			msg.h.nlmsg_len += 2 * sizeof(msg.a6[0]);
+		}
+		return __nlinline_conversation(&msg);
+	}
+}
+
+static inline int nlinline_ipaddr_add(int family, void *addr, int prefixlen, int ifindex) {
+	return __nlinline_ipaddr(RTM_NEWADDR, NLM_F_EXCL | NLM_F_CREATE, family, addr, prefixlen, ifindex);
+}
+
+static inline int nlinline_ipaddr_del(int family, void *addr, int prefixlen, int ifindex) {
+	return __nlinline_ipaddr(RTM_DELADDR, 0, family, addr, prefixlen, ifindex);
+}
+
+static inline int __nlinline_iproute(int request, int xflags, int family, void *dst_addr, int dst_prefixlen, void *gw_addr) {
+	int addrlen = nlinline_family2addrlen(family);
+  if (addrlen == 0)
+    return errno = EINVAL, -1;
+  else {
+    struct {
+      struct nlmsghdr h;
+      struct rtmsg r;
+      union {
+        struct __nlinline_ipv4attr a4[2];
+        struct __nlinline_ipv6attr a6[2];
+      };
+    } msg = {
+      .h.nlmsg_len = sizeof(msg.h) + sizeof(msg.r) + 2 * sizeof(struct nlattr) + 2 * addrlen,
+      .h.nlmsg_type = request,
+      .h.nlmsg_flags =  NLM_F_REQUEST | NLM_F_ACK | xflags,
+      .h.nlmsg_seq = 1,
+      .r.rtm_family = family,
+      .r.rtm_dst_len = dst_prefixlen,
+			.r.rtm_table = RT_TABLE_MAIN,
+			.r.rtm_protocol = RTPROT_BOOT,
+			.r.rtm_scope = RT_SCOPE_UNIVERSE,
+			.r.rtm_type = RTN_UNICAST};
+		int nattr = 0;
+    if (addrlen == 4) {
+			if (dst_prefixlen > 0) {
+				msg.a4[nattr].h.nla_len = sizeof(struct nlattr) + addrlen;
+				msg.a4[nattr].h.nla_type = RTA_DST;
+				msg.a4[nattr].addr = *((struct __nlinline_ipv4addr *)dst_addr);
+				nattr++;
+			}
+			msg.a4[nattr].h.nla_len = sizeof(struct nlattr) + addrlen;
+			msg.a4[nattr].h.nla_type = RTA_GATEWAY;
+			msg.a4[nattr].addr = *((struct __nlinline_ipv4addr *)gw_addr);
+			nattr++;
+			msg.h.nlmsg_len += nattr * sizeof(msg.a4[0]);
+    } else {
+			if (dst_prefixlen > 0) {
+				msg.a6[nattr].h.nla_len = sizeof(struct nlattr) + addrlen;
+				msg.a6[nattr].h.nla_type = RTA_DST;
+				msg.a6[nattr].addr = *((struct __nlinline_ipv6addr *)dst_addr);
+				nattr++;
+			}
+			msg.a6[nattr].h.nla_len = sizeof(struct nlattr) + addrlen;
+			msg.a6[nattr].h.nla_type = RTA_GATEWAY;
+			msg.a6[nattr].addr = *((struct __nlinline_ipv6addr *)gw_addr);
+			nattr++;
+			msg.h.nlmsg_len += nattr * sizeof(msg.a6[0]);
+		}
+		return __nlinline_conversation(&msg);
+	}
+}
+
+static inline int nlinline_iproute_add(int family, void *dst_addr, int dst_prefixlen, void *gw_addr) {
+	return __nlinline_iproute(RTM_NEWROUTE, NLM_F_EXCL | NLM_F_CREATE, family, dst_addr, dst_prefixlen, gw_addr);
+}
+
+static inline int nlinline_iproute_del(int family, void *dst_addr, int dst_prefixlen, void *gw_addr) {
+	return __nlinline_iproute(RTM_DELROUTE, 0, family, dst_addr, dst_prefixlen, gw_addr);
+}
+#endif
